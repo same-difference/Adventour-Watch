ee0977e61ddcb8efd1f6380247d23b21bb4889b9
same-difference <dnmurillo@gmail.com>
2025-04-08 21:45:36 -0400

    FINALLY TEXT ON A SCREEN

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
 #include <Arduino.h>
 #include <WiFi.h>
 #include <time.h>
-#include "secrets.h"
+#include "secrets.h" // Define WIFI_SSID and WIFI_PASSWORD
+#include <TFT_eSPI.h>
+#include <Wire.h>
+
+// Pin defs (match User_Setup.h)
+#define TOUCH_SDA   6
+#define TOUCH_SCL   7
+#define TOUCH_INT   5
+#define TOUCH_RST  13
+#define TFT_BL      2
+
+TFT_eSPI tft = TFT_eSPI();
 
 const long gmtOffset_sec = -5 * 3600;
 const int daylightOffset_sec = 3600;
@@ -10,6 +21,7 @@ void setup() {
   Serial.begin(115200);
   delay(1000);
 
+  // Start WiFi
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   Serial.print("Connecting to WiFi");
   while (WiFi.status() != WL_CONNECTED) {
@@ -18,6 +30,7 @@ void setup() {
   }
   Serial.println(" connected!");
 
+  // Set system time
   configTime(gmtOffset_sec, daylightOffset_sec, "pool.ntp.org");
 
   struct tm timeinfo;
@@ -25,6 +38,29 @@ void setup() {
     Serial.println("Failed to obtain time");
     return;
   }
+
+  // Init I2C for touchscreen
+  Wire.begin(TOUCH_SDA, TOUCH_SCL);
+  pinMode(TOUCH_RST, OUTPUT);
+  pinMode(TOUCH_INT, INPUT);
+
+  // Reset touch chip
+  digitalWrite(TOUCH_RST, LOW);
+  delay(100);
+  digitalWrite(TOUCH_RST, HIGH);
+  delay(100);
+
+  // Turn on backlight
+  pinMode(TFT_BL, OUTPUT);
+  digitalWrite(TFT_BL, HIGH);
+
+  // Init display
+  tft.init();
+  tft.setRotation(0);
+  tft.fillScreen(TFT_BLACK);
+  tft.setTextColor(TFT_WHITE);
+  tft.setTextDatum(MC_DATUM);
+  tft.setTextSize(2);
 }
 
 void loop() {
@@ -34,19 +70,24 @@ void loop() {
     if (hour12 == 0) hour12 = 12;
     const char* ampm = (timeinfo.tm_hour < 12) ? "AM" : "PM";
 
-    char line1[16];
-    char line2[16];
+    char dateStr[16];
+    char timeStr[16];
 
     // date formatting
-    snprintf(line1, sizeof(line1), "%02d/%02d/%04d", 
+    snprintf(dateStr, sizeof(dateStr), "%02d/%02d/%04d", 
              timeinfo.tm_mon + 1, timeinfo.tm_mday, timeinfo.tm_year + 1900);
 
     // tiem formatting
-    snprintf(line2, sizeof(line2), "%02d:%02d:%02d %s", 
+    snprintf(timeStr, sizeof(timeStr), "%02d:%02d:%02d %s", 
              hour12, timeinfo.tm_min, timeinfo.tm_sec, ampm);
 
-    Serial.println(line1);
-    Serial.println(line2);
+    // Display text centered
+    tft.fillScreen(TFT_BLACK);
+    tft.drawString(dateStr, 120, 105);   // Center of screen
+    tft.drawString(timeStr, 120, 135);
+
+    Serial.println(dateStr);
+    Serial.println(timeStr);
     Serial.println();
   } else {
     Serial.println("Failed to get time");
